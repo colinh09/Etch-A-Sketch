934aa5ff2502dfab20f0eba7e5164e6424688d30
Colin Hwang <colin.hwang@cooper.edu>
2021-08-22 23:03:04 -0400

    Wip

--- myscripts.js
+++ myscripts.js
@@ -10,6 +10,24 @@ heading.appendChild(h1);
 
 const container = document.createElement("#container");
 
-const grid = documnet.createElement("#container");
+const grid = document.createElement("#grid");
+
+function createGrid(size)
+{
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size * size; i++)
+    {
+        let div = document.createElement("div");
+        div.classList.add('cell');
+        div.style.width = `${size}px`;
+        div.style.height = `${size}px`;
+        grid.appendChild(div);
+    }
+}
+
+container.appendChild(grid);
+
+createGrid(16);
 
 
